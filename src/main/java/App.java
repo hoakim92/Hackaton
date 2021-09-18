e174d12372330cb5add6bab21b2639dba9849347
hoakim92 <hoakim92@gmail.com>
2021-09-18 14:46:48 +0300

    Add second test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,11 @@
 public class App {
     public static void main(String[] args) {
         try {
-            cucumber.api.cli.Main.main(args);
+            if (args.length == 0) {
+                cucumber.api.cli.Main.main(new String[]{"-g", "", "src/test/features"});
+            } else {
+                cucumber.api.cli.Main.main(args);
+            }
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
--- src/test/features/test.feature
+++ src/test/features/test.feature
@@ -1,10 +1,12 @@
 Feature: Test
 
+  @FirstThread
   Scenario: One
     Given Open Store
     Then Click on '/html/body/main/div[3]/div[2]/a/img'
     Then Validate title 'Streetwear | Collections | Apparel Site UK'
 
+  @SecondThread
   Scenario: Two
     Given Open Store
     Then Click on '/html/body/main/div[3]/div[2]/a/img'
